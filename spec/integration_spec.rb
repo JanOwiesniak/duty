d7fd585415824b94cffb65ab5085d44ddd09f891
Jan Owiesniak <owiesniak@mailbox.org>
2015-11-09 20:33:42 +0100

    Close message with single line break

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -4,13 +4,13 @@ require "open3"
 class IntegrationSpec < MiniTest::Spec
   describe 'without a command' do
     it 'explains how to use the executable' do
-      assert_stdout /usage: duty <command> \[<args>\]\s{2}/ do
+      assert_stdout /usage: duty <command> \[<args>\]\s{1}/ do
         exec('bin/duty')
       end
     end
 
     it 'lists all available commands' do
-      assert_stdout /Commands:\s{2}new-feature\tCreates a new feature branch/ do
+      assert_stdout /Commands:\s{2}new-feature\tCreates a new feature branch\s{1}/ do
         exec('bin/duty')
       end
     end
@@ -18,7 +18,7 @@ class IntegrationSpec < MiniTest::Spec
 
   describe 'with unknown command' do
     it 'explains that the given command is invalid' do
-      assert_stdout /duty: `foo bar` is not a duty command\s{2}/ do
+      assert_stdout /duty: `foo bar` is not a duty command\s{1}/ do
         exec('bin/duty foo bar')
       end
     end
@@ -27,13 +27,13 @@ class IntegrationSpec < MiniTest::Spec
   describe 'with known command' do
     describe 'new-feature' do
       it 'describes the command' do
-        assert_stdout /Creates a new feature branch\s{2}/ do
+        assert_stdout /Creates a new feature branch\s{1}/ do
           exec('bin/duty new-feature')
         end
       end
 
       it 'explains how to use the command' do
-        assert_stdout /usage: duty new-feature <name>\s{2}/ do
+        assert_stdout /usage: duty new-feature <name>\s{1}/ do
           exec('bin/duty new-feature')
         end
       end
