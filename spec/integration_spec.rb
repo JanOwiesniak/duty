ad42caa7a298bf39137a64167d9c70051a5aebbe
Jan Owiesniak <owiesniak@mailbox.org>
2015-11-09 19:32:39 +0100

    Show useage section on stdout

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1,7 +1,23 @@
 require "minitest/autorun"
+require "open3"
 
 class IntegrationSpec < MiniTest::Spec
-  it 'runs the test suite' do
-    assert true
+  describe 'without a command' do
+    it 'explains how to use the executable' do
+      assert_stdout /(usage: duty <command> \[<args>\]\s+)/ do
+        exec('bin/duty')
+      end
+    end
+  end
+
+  private
+
+  def assert_stdout(expected, &command)
+    stdout, stderr, status = yield command
+    assert_match expected, stdout
+  end
+
+  def exec(command)
+    Open3.capture3(command)
   end
 end
