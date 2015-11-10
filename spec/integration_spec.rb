a6a4b29dc181832721797ca1af62a016d34f5ca8
Jan Owiesniak <owiesniak@mailbox.org>
2015-11-10 08:14:53 +0100

    Add executable to $PATH

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -5,13 +5,13 @@ class IntegrationSpec < MiniTest::Spec
   describe 'without a command' do
     it 'explains how to use the executable' do
       assert_stdout /usage: duty <command> \[<args>\]\s{1}/ do
-        exec('bin/duty')
+        exec('duty')
       end
     end
 
     it 'lists all available commands' do
       assert_stdout /Commands:\s{2}new-feature\tCreates a new feature branch\s{1}/ do
-        exec('bin/duty')
+        exec('duty')
       end
     end
   end
@@ -19,7 +19,7 @@ class IntegrationSpec < MiniTest::Spec
   describe 'with unknown command' do
     it 'explains that the given command is invalid' do
       assert_stdout /duty: `foo bar` is not a duty command\s{1}/ do
-        exec('bin/duty foo bar')
+        exec('duty foo bar')
       end
     end
   end
@@ -29,13 +29,13 @@ class IntegrationSpec < MiniTest::Spec
       describe 'without name' do
         it 'describes the command' do
           assert_stdout /Creates a new feature branch\s{1}/ do
-            exec('bin/duty new-feature')
+            exec('duty new-feature')
           end
         end
 
         it 'explains how to use the command' do
           assert_stdout /usage: duty new-feature <name>\s{1}/ do
-            exec('bin/duty new-feature')
+            exec('duty new-feature')
           end
         end
       end
@@ -43,31 +43,31 @@ class IntegrationSpec < MiniTest::Spec
       describe 'with name' do
         it 'explains what just happend' do
           assert_stdout /What just happend:\s{2}/ do
-            exec('bin/duty new-feature my-awesome-feature')
+            exec('duty new-feature my-awesome-feature')
           end
         end
 
         it 'checks out the `master` branch' do
           assert_stdout /Checked out `master` branch\s{1}/ do
-            exec('bin/duty new-feature my-awesome-feature')
+            exec('duty new-feature my-awesome-feature')
           end
         end
 
         it 'creates a new feature branch' do
           assert_stdout /Created new feature branch `feature\/my-awesome-feature`\s{1}/ do
-            exec('bin/duty new-feature my-awesome-feature')
+            exec('duty new-feature my-awesome-feature')
           end
         end
 
         it 'checks out the new feature branch' do
           assert_stdout /Checked out new feature branch `feature\/my-awesome-feature`\s{1}/ do
-            exec('bin/duty new-feature my-awesome-feature')
+            exec('duty new-feature my-awesome-feature')
           end
         end
 
         it 'pushs new feature branch to origin' do
           assert_stdout /Pushed new feature branch `feature\/my-awesome-feature` to `origin`\s{1}/ do
-            exec('bin/duty new-feature my-awesome-feature')
+            exec('duty new-feature my-awesome-feature')
           end
         end
       end
@@ -78,11 +78,15 @@ class IntegrationSpec < MiniTest::Spec
 
   def assert_stdout(expected, &command)
     stdout, stderr, status = yield command
-    assert_match expected, stdout
-    assert_equal true, status.success?
+    assert_match expected, stdout, "Expected stdout to be #{expected} but got #{stdout}"
+    assert_equal true, status.success?, "Expected status to be 0 but got #{status}"
   end
 
   def exec(command)
-    Open3.capture3(command)
+    capture(command)
+  end
+
+  def capture(command, options = {})
+    stdout, stderr, status = Open3.capture3(command, options)
   end
 end
