91b5bbae4c53c578e84d094cef762804663bb176
Jan Owiesniak <owiesniak@mailbox.org>
2015-11-13 09:09:32 +0100

    Do not crash on Open3 exceptions
    Instead present the error in the CLI

--- lib/duty/system.rb
+++ lib/duty/system.rb
@@ -3,7 +3,11 @@ require 'open3'
 module Duty
   class System
     def call(cmd)
-      Open3.capture3(cmd)
+      begin
+        Open3.capture3(cmd)
+      rescue Exception => e
+        [nil, e.message, -1]
+      end
     end
   end
 end
