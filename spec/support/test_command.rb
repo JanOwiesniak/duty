d8217579c603e1a28be41b67745c440541777f2a
Jan Owiesniak <owiesniak@mailbox.org>
2015-11-22 11:43:55 +0100

    Extract test task specific commands into reuseable commands module

--- spec/support/test_command.rb
+++ spec/support/test_command.rb
@@ -1,15 +1,37 @@
 module Duty
+  module Commands
+    module Test
+      def shell_commands(test_type)
+        sh {}
+        sh("First #{test_type} shell command") { 'pwd' }
+        sh("Second #{test_type} shell command") { 'boom' }
+        sh("Third #{test_type} shell command") { 'pwd' }
+      end
+
+      def ruby_commands(test_type)
+        ruby {}
+        ruby("First #{test_type} ruby command") {}
+        ruby("Second #{test_type} ruby command") { raise RuntimeError.new }
+        ruby("Third #{test_type} ruby command") {}
+      end
+    end
+  end
+end
+
+module Duty
   module Tasks
     class Test < Base
+      include Duty::Commands::Test
+
       def execute
         if sequential? 
-          shell_commands if shell?
-          ruby_commands if ruby?
+          shell_commands(test_type) if shell?
+          ruby_commands(test_type) if ruby?
         end
 
         if parallel? 
-          parallel { shell_commands }
-          parallel { ruby_commands }
+          parallel { shell_commands(test_type) }
+          parallel { ruby_commands(test_type) }
         end
       end
 
@@ -41,20 +63,6 @@ module Duty
 
       def test_command
         @arguments[1]
-      end
-
-      def shell_commands
-        sh {}
-        sh("First #{test_type} shell command") { 'pwd' }
-        sh("Second #{test_type} shell command") { 'boom' }
-        sh("Third #{test_type} shell command") { 'pwd' }
-      end
-
-      def ruby_commands
-        ruby {}
-        ruby("First #{test_type} ruby command") {}
-        ruby("Second #{test_type} ruby command") { raise RuntimeError.new }
-        ruby("Third #{test_type} ruby command") {}
       end
     end
   end
